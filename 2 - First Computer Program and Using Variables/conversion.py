b083df7f36580b607af238ba3033fbea7244d3ce
Braddyers <braddyers@gmail.com>
2026-06-22 21:19:56 +0200

    Add Task 2 Files

---
+++ 2 - First Computer Program and Using Variables/conversion.py
@@ -0,0 +1,28 @@
+# set the given values to their respective given variables:
+# 99.23 to num1
+# 23 to num2
+# 150 to num3
+# "100" to string1
+
+# Then cast each variable as follows:
+# num1 into an integer
+# num2 into a float
+# num3 into a string
+# string1 into an integer
+
+# output each variable on separate lines
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+num1 = int(num1)
+num2 = float(num2)
+num3 = str(num3)
+string1 = int(string1)
+
+print(num1)
+print(num2)
+print(num3)
+print(string1)
---
+++ 2 - First Computer Program and Using Variables/hello_world.py
@@ -0,0 +1,15 @@
+# request that the user inputs their name
+# then output their name
+
+# request that the user inputs their age
+# then output their age
+
+# output the string "Hello World!"
+
+user_name = input("Please enter your name: ")
+print(user_name)
+
+user_age = input("Please enter your age: ")
+print(user_age)
+
+print("Hello World!")
