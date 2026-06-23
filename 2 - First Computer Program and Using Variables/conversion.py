3cae79577a59f3ac87446faf99e03ba738049313
Braddyers <braddyers@gmail.com>
2026-06-23 12:47:14 +0200

    Edited comments

--- 2 - First Computer Program and Using Variables/conversion.py
+++ 2 - First Computer Program and Using Variables/conversion.py
@@ -4,23 +4,23 @@
 # 150 to num3
 # "100" to string1
 
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
 # Then cast each variable as follows:
 # num1 into an integer
 # num2 into a float
 # num3 into a string
 # string1 into an integer
 
-# output each variable on separate lines
-
-num1 = 99.23
-num2 = 23
-num3 = 150
-string1 = "100"
-
 num1 = int(num1)
 num2 = float(num2)
 num3 = str(num3)
 string1 = int(string1)
+
+# output each variable on separate lines
 
 print(num1)
 print(num2)
