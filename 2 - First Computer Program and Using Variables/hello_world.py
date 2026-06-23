79a28b0081f85d202d98e07f2e4f6284fba1f09b
Braddyers <braddyers@gmail.com>
2026-06-23 12:53:47 +0200

    Edited comments

--- 2 - First Computer Program and Using Variables/hello_world.py
+++ 2 - First Computer Program and Using Variables/hello_world.py
@@ -1,15 +1,15 @@
 # request that the user inputs their name
 # then output their name
 
-# request that the user inputs their age
-# then output their age
-
-# output the string "Hello World!"
-
 user_name = input("Please enter your name: ")
 print(user_name)
 
+# request that the user inputs their age
+# then output their age
+
 user_age = input("Please enter your age: ")
 print(user_age)
+
+# output the string "Hello World!"
 
 print("Hello World!")
