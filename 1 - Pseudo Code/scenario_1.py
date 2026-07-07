86df71043c8b94a719e6f8b3633064d30846d8ef
Braddyers <braddyers@gmail.com>
2026-07-07 15:39:44 +0200

    Update comments in scenario_1 py file

--- 1 - Pseudo Code/scenario_1.py
+++ 1 - Pseudo Code/scenario_1.py
@@ -4,34 +4,35 @@
 # until the user enters a zero value, then determines and outputs the
 # largest of the numbers that were input.
 
+# Create an empty list to store the numbers
 number_list = []
 
-
+# Create a loop that will continue until the user enters 0 
 while True:
     number_input = input("Please enter a number > 0 or enter 0 to stop: ")
     
-    
+    # Check if the input is an integer
     if number_input.isdigit():
         number_input = int(number_input)
         
-        
+        # Check if the input is 0, if so, break the loop
         if number_input == 0:
             print("You have entered 0. The program will now end.")
             break
         
-        
+        # Check if the input is a positive integer, if so, add it to the list
         elif number_input > 0:
             number_list.append(number_input)
             
-            
+            # A check if the list is correctly storing the numbers 
             print(number_list)
             
-            
+            # Inform the user that their positive integer has been added to the list
             print(f"'{number_input}' has been added to the list.")
 
+            # Inform the user of the largest number in the list
+            print(f"The largest number on the list is: {max(number_list)}.")
             
-            print(f"The largest number on the list currently is: {max(number_list)}.")
-            
-    
+    # Inform the user that that is not a positive integer and request another input
     else:
         print("You have not entered a positive number. Please try again.")
