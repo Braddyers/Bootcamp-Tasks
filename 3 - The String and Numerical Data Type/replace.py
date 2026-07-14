f5d82893b56b638e23445c91412b5c0b94b969f6
Braddyers <braddyers@gmail.com>
2026-07-14 02:48:33 +0200

    Edided comments in replace.py file

--- 3 - The String and Numerical Data Type/replace.py
+++ 3 - The String and Numerical Data Type/replace.py
@@ -1,13 +1,23 @@
+# Store the following string in a variable: "The!quick!brown!fox!jumps!over!the!lazy!dog."
 sentence1 = "The!quick!brown!fox!jumps!over!the!lazy!dog."
 
+# Replace all the "!" with a space
+# Store the string in a new variable
 sentence2 = sentence1.replace("!", " ")
 
+# Output the modified string
 print(sentence2)
 
+# Convert the modified string to all uppercase letters
+# Store it in a new variable
 sentence3 = sentence2.upper()
 
+# Output the modified string
 print(sentence3)
 
+# Reverse the modified string
+# Store it in a new variable
 sentence4 = sentence3[ : :-1]
 
+# Output the modified string
 print(sentence4)
