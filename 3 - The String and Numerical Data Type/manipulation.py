9f6ff2649330004ddd6bf16a185bb8adbf5fa047
Braddyers <braddyers@gmail.com>
2026-07-14 16:14:15 +0200

    Edited comments of many files

--- 3 - The String and Numerical Data Type/manipulation.py
+++ 3 - The String and Numerical Data Type/manipulation.py
@@ -1,9 +1,7 @@
 # Request the user to input a string
-# Store the string in a variable
 str_manip = (input("What is the most famous monument in China? "))
 
 # Calculate the length of the string
-# Store the integer in a new variable
 str_length = len(str_manip)
 
 # Output the length of the string
@@ -13,7 +11,6 @@ print(f"The character length of your answer is: {str_length}")
 last_letter = str_manip[-1]
 
 # Replace every occurance of that letter with @
-# Store the modified string in variable
 str_manip = str_manip.replace(last_letter, "@")
 
 # Output the modified string
@@ -21,7 +18,6 @@ print(f"Your answer with every occurrence of its last " +
       f"letter replaced with @ is: {str_manip}")
 
 # Find the last three characters of the string in reverse order
-# Store the string in a new variable
 last_3_inrev = str_manip[ :-4:-1]
 
 # Output the last three characters of the string in reverse order
@@ -29,15 +25,12 @@ print(f"The last three characters of your answer " +
       f"in reverse order is: {last_3_inrev}")
 
 # Find the first three letters of the string
-# Store that new string in a new variable
 first_3_char = str_manip[:3]
 
 # Find the last two letters of the string
-# Store that new string in a new variable
 last_2_char = str_manip[-2:]
 
 # Concatinate the two variables to make a new string
-# Store the new string in a new variable 
 five_letter_word = first_3_char + last_2_char
 
 # Output the new string
