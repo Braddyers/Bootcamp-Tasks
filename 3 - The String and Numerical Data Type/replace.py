fd2855ec13fba631c2b1b4b1a315719fa28a66b2
Braddyers <braddyers@gmail.com>
2026-07-14 16:23:47 +0200

    Edited lengths of code to stick to PEP 8

--- 3 - The String and Numerical Data Type/replace.py
+++ 3 - The String and Numerical Data Type/replace.py
@@ -3,21 +3,18 @@
 sentence1 = "The!quick!brown!fox!jumps!over!the!lazy!dog."
 
 # Replace all the "!" with a space
-# Store the string in a new variable
 sentence2 = sentence1.replace("!", " ")
 
 # Output the modified string
 print(sentence2)
 
 # Convert the modified string to all uppercase letters
-# Store modified string in a new variable
 sentence3 = sentence2.upper()
 
 # Output the modified string
 print(sentence3)
 
 # Reverse the modified string
-# Store it in a new variable
 sentence4 = sentence3[ : :-1]
 
 # Output the modified string
