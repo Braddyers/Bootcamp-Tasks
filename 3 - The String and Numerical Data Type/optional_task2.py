04796cc4e8dc0ad60bf1127da6c93220cfc7c89c
Braddyers <braddyers@gmail.com>
2026-07-16 17:24:33 +0200

    Edited Task 3, optional_task2

--- 3 - The String and Numerical Data Type/optional_task2.py
+++ 3 - The String and Numerical Data Type/optional_task2.py
@@ -1,9 +1,16 @@
+# Request the user input the name of their favourite restaurant
 string_fav = input("Enter the name of your favourite restaurant: ")
 
+# Request the user input the name of their favourite number
+# Cast it into and integer
 int_fav = int(input("Enter your favourite number: "))
 
+# Output the inputs
 print(f"Your favourite restaurant is {string_fav}.")
 print(f"Your favourite number is {int_fav}.")
 
+# Cast string_fav into an integer then output the results
 print(int(string_fav))
-# an error was returned as string_fav is a string made up of words (a string literal) with no numercal value and cannot be cast into an integer.
+
+# An error was returned as string_fav is a string made up of words (string 
+# literals) with no numercal value and cannot be cast into an integer
